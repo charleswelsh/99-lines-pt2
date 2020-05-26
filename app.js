cf5b89c96d1ddce5991f4944b9d9066baa945b33
Charles Welsh <charleswelsh@gmail.com>
2020-05-26 10:07:02 -0500

    fixed color of div text

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 let friendList= ["Dash", "Dan", "Chase", "Ben", "Alex"];
-for (let i = 0; i < friendList.length; i++) {
-    console.log(friendList[i] + ":")
-    for (let n = 99; n > 0; n--) {
-        if (n === 2) {
-            console.log(`${n} lines of code in the file, ${n} lines of code; ${friendList[i]} strikes one out, clears it all out, ${n - 1} line of code in the file`);   
+document.addEventListener("DOMContentLoaded", function(){
+    let btnSing = document.createElement ("button");
+    btnSing.innerText = "Sing!";
+    document.body.appendChild (btnSing);
+    btnSing.addEventListener ("click", function() {
+        for (let i = 0; i < friendList.length; i++) {
+            let friendDiv = document.createElement ("div");
+            document.body.appendChild (friendDiv);
+
+            let friendName = document.createElement ("h3")
+            friendName.innerText = (friendList[i] + ":")
+            friendDiv.appendChild (friendName);
+
+            for (let n = 99; n > 0; n--) {
+                let songText = document.createElement ("p");
+                friendDiv.appendChild (songText);
+
+                if (n === 2) {
+                    songText.innerText = (`${n} lines of code in the file, ${n} lines of code; ${friendList[i]} strikes one out, clears it all out, ${n - 1} line of code in the file`);   
+                }
+                else if (n === 1) {
+                    songText.innerText = (`${n} line of code in the file, ${n} line of code; ${friendList[i]} strikes one out, clears it all out, 0 lines of code in the file`);            
+                }
+                else {
+                    songText.innerText = (`${n} lines of code in the file, ${n} lines of code; ${friendList[i]} strikes one out, clears it all out, ${n - 1} lines of code in the file`);
+                }        
+            }
         }
-        else if (n === 1) {
-            console.log(`${n} line of code in the file, ${n} line of code; ${friendList[i]} strikes one out, clears it all out, 0 lines of code in the file`);            
-        }
-        else {
-            console.log(`${n} lines of code in the file, ${n} lines of code; ${friendList[i]} strikes one out, clears it all out, ${n - 1} lines of code in the file`);
-        }        
-    }
-}
+    });
+});
